28662f37a32189bad1de3182f6f13a8595781784
bsong1124 <bsong1124@gmail.com>
2024-01-20 06:27:01 -0500

    comment out default css, create new dirs and respective files, remove unnecessary files

---
+++ capstone-client/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+      <nav>
+        <Link>Header</Link>
+        <Link>Comp</Link>
+      </nav>
+  );
+};
+
+export default Header;
---
+++ capstone-client/src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div>
+      <Routes>
+      </Routes>
+      <h1>Main Comp</h1>
+    </div>
+  );
+};
+
+export default Main;
