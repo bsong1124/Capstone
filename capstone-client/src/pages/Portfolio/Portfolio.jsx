8695bea51e7634d1690483aed5b4f23bd6ec6197
bsong1124 <bsong1124@gmail.com>
2024-01-21 16:37:18 -0500

    delete user code

--- capstone-client/src/pages/Portfolio/Portfolio.jsx
+++ capstone-client/src/pages/Portfolio/Portfolio.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 
-const authId = user.sub.substring(user.sub.indexOf("|") + 1);
 const Portfolio = () => {
   const { user, isAuthenticated } = useAuth0();
   const [portfolio, setPortfolio] = useState([])
