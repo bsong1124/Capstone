c6bd7283c89ef843b060c3fda851b544212402ef
bsong1124 <bsong1124@gmail.com>
2024-01-21 08:46:44 -0500

    update Home page to render popular coins

--- capstone-client/src/utilities/coins-api.js
+++ capstone-client/src/utilities/coins-api.js
@@ -1,2 +1,17 @@
 import config from "../config";
-  
+
+export async function getPopularCoins() {
+    const popularResponse = await fetch(`${config.BASE_URL}/popular`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (popularResponse.ok) {
+      const popularData = await popularResponse.json();
+      console.log(popularData.coins)
+      return popularData.coins;
+    } else {
+      console.log(err.message);
+    }
+  }
--- capstone-client/src/utilities/coins-service.js
+++ capstone-client/src/utilities/coins-service.js
@@ -1,1 +1,11 @@
 import * as coinsApi from './coins-api'
+
+export async function findPopular() {
+    try {
+      const popular = await coinsApi.getPopularCoins();
+      return popular;
+    } catch (err) {
+      console.log(err.message);
+      throw err;
+    }
+  }
