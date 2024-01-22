65e28888e705437aa0af62482fb3c0106a344ff0
bsong1124 <bsong1124@gmail.com>
2024-01-22 04:06:59 -0500

    minimal css update, edit nav bar

--- capstone-client/src/components/Header/Header.jsx
+++ capstone-client/src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <nav>
       <Link to="/">Home</Link>
-      <Link to="/coins">Coins</Link>
+      <Link to="/coins">Search</Link>
       <Link to="/portfolio">Portfolio</Link>
       {/* <Link to="/profile">Profile</Link> */}
       <Link to="/about">About</Link>
