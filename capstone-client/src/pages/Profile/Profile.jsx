673ba6db9a88961709b2d925f9bb7faa2b963c42
bsong1124 <bsong1124@gmail.com>
2024-01-21 21:42:51 -0500

    fix getProfile function, add form to edit/update profile info

--- capstone-client/src/pages/Profile/Profile.jsx
+++ capstone-client/src/pages/Profile/Profile.jsx
@@ -5,8 +5,9 @@ import { createMyProfile, getMyProfile } from "../../utilities/coins-service";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
-  // console.log('EMAIL', user.email)
   const [profile, setProfile] = useState([]);
+  const [editName, setEditName] = useState("");
+  const [editEmail, setEditEmail] = useState("");
 
   const createProfile = async () => {
     const authId = user.sub.substring(user.sub.indexOf("|") + 1);
@@ -14,20 +15,43 @@ const Profile = () => {
     try {
       const create = await createMyProfile({
         googleId: authId,
-        name: user.given_name + user.family_name,
+        name: `${user.given_name} + ${user.family_name}`,
         email: user.email,
         portfolio: [],
       });
+      //   console.log("USER NAME", create.name);
+      const userProfile = await getMyProfile(authId);
+      setProfile(userProfile);
+      //   console.log("WORKING");
+      //   console.log("working");
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  //   console.log({user})
+
+  const getProfile = async () => {
+    const authId = user.sub.substring(user.sub.indexOf("|") + 1);
+    try {
       const userProfile = await getMyProfile(authId);
       setProfile(userProfile);
     } catch (err) {
       console.log(err.message);
     }
   };
+  console.log({ profile });
 
   useEffect(() => {
     createProfile();
+    getProfile();
   }, []);
+
+  const handleChange = () => {
+    setEditName(e.target.value);
+    setEditEmail(e.target.value);
+  };
+
+  const editProfile = async () => {};
 
   if (!isAuthenticated) {
     return <Navigate to="/" />;
@@ -38,15 +62,24 @@ const Profile = () => {
   }
 
   return (
-    isAuthenticated && profile && (
+    isAuthenticated &&
+    profile && (
       <div>
         <img src={user.picture} />
-        <h2>{user.name}</h2>
+        <h2>{profile.name}</h2>
         <p>{profile.email}</p>
-        <form>
-          <input placeholder="Edit Name" />
-          <input placeholder="Edit Email" />
-          <button></button>
+        <form onSubmit={editProfile}>
+          <input
+            value={editName}
+            onChange={handleChange}
+            placeholder="Edit Name"
+          />
+          <input
+            value={editEmail}
+            onChange={handleChange}
+            placeholder="Edit Email"
+          />
+          <button type="submit">Edit</button>
         </form>
       </div>
     )
