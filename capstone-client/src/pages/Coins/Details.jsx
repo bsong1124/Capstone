f92560606ea03ec87537037f14b85fe2728cd00d
bsong1124 <bsong1124@gmail.com>
2024-01-22 08:46:47 -0500

    implement chartJS in Details page, write helper functions for all data rendered in Details

--- capstone-client/src/pages/Coins/Details.jsx
+++ capstone-client/src/pages/Coins/Details.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { findLayerOne, searchCoin } from "../../utilities/coins-service";
 import { getCoinDetails } from "../../utilities/coins-service";
+
+import { Chart as ChartJS } from "chart.js/auto";
+import { Line, Bar, Doughnut } from "react-chartjs-2";
+// import { Utils } from "chartjs-adapter-date-fns";
 
 import "./coins.css";
 
 const Coins = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [coinDetails, setCoinDetails] = useState([]);
+  const [priceRangeData, setPriceRangeData] = useState([]);
 
   const { id } = useParams();
 
@@ -22,14 +26,16 @@ const Coins = () => {
     // const
     try {
       const detailResponse = await getCoinDetails(id);
-      setCoinDetails(detailResponse);
+      //   console.log("PRICE RANGE", detailResponse.priceRangeData);
+      setCoinDetails(detailResponse.coinData);
+      setPriceRangeData(detailResponse.priceRangeData);
       setIsLoading(false);
     } catch (err) {
       console.log(err.message);
     }
   };
 
-  //   console.log({ coinDetails });
+  console.log({ priceRangeData });
 
   const renderDetails = () => {
     const c = { ...coinDetails };
@@ -59,97 +65,167 @@ const Coins = () => {
       return formattedChange;
     };
 
-    // console.log("PRICE DIFFERENCE", getPriceDifference(41000, 68000));
-    // console.log("PRICE DIFFERENCE", getPriceDifference(41000, 67.81));
+    const formatUnix = (unixMilliseconds) => {
+        // Create a new Date object using the Unix timestamp in milliseconds
+        const date = new Date(unixMilliseconds);
+      
+        // Get the month, day, hour, and minute from the date
+        const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Add 1 to month because it is zero-based
+        const day = date.getDate().toString().padStart(2, "0");
+        const hour = date.getHours().toString().padStart(2, "0");
+        const minute = date.getMinutes().toString().padStart(2, "0");
+      
+        // Format the date to "mm/dd"
+        const formattedDate = `${month}/${day}`;
+        const formattedTime = `${hour}:${minute}`;
+      
+        return { formattedDate, formattedTime };
+      };
+
+    const skipped = (ctx, value) =>
+      ctx.p0.skip || ctx.p1.skip ? value : undefined;
+    const down = (ctx, value) =>
+      ctx.p0.parsed.y > ctx.p1.parsed.y ? value : undefined;
+
+    const genericOptions = {
+      fill: false,
+      interaction: {
+        intersect: false,
+      },
+      radius: 4,
+    };
+
+    const data = {
+      type: "line",
+      data: {
+        labels: priceRangeData.map((p) => [
+          formatUnix(p[0]).formattedDate,
+          formatUnix(p[0]).formattedTime,
+        ]),
+        datasets: [
+          {
+            label: "Price Chart",
+            data: priceRangeData.map((p) => p[1]),
+            borderColor: "rgb(75, 192, 192)",
+            color: "black",
+            backgroundColor: (ctx) => {
+              const index = ctx.dataIndex;
+              const value = ctx.dataset.data[index];
+              const previousValue =
+                index > 0 ? ctx.dataset.data[index - 1] : null;
+              return previousValue !== null && value > previousValue
+                ? "rgb(75, 192, 192)"
+                : "rgb(192,75,75)";
+            },
+            segment: {
+              borderColor: (ctx) =>
+                skipped(ctx, "rgb(0,0,0)") || down(ctx, "rgb(192,75,75)"),
+              borderDash: (ctx) => skipped(ctx, [6, 6]),
+            },
+            spanGaps: true,
+          },
+        ],
+      },
+      options: genericOptions,
+    };
+
+    const ChartComponent = () => (
+      <div className="bar">
+        <Line data={data.data} options={data.options} />
+      </div>
+    );
 
     return (
       <div>
-        {coinDetails && (
-          <div>
-            <p>
-              Name: {c.name} <img className="coin-img" src={c.image.thumb} />
-            </p>
-            <p>Id: {c.id}</p>
-            <p>Ticker: {c.symbol}</p>
-            <p>Description: {c.description.en || "N/A"}</p>
-            {c?.links && (
-              <div>
-                <p>
-                  Homepage:{" "}
-                  <a href={c.links?.homepage?.[0]}>
-                    {" "}
-                    {c.links.homepage || "N/A"}
-                  </a>
-                </p>
-                <p>
-                  Whitepaper:{" "}
-                  <a href={c.links?.whitepaper?.[0]}>
-                    {c.links.whitepaper || "N/A"}
-                  </a>
-                </p>
-                <p>
-                  Reddit:{" "}
-                  <a href={c.links?.subreddit_url?.[0]}>
-                    {c.links.subreddit_url || "N/A"}
-                  </a>
-                </p>
-                <p>
-                  Github:{" "}
-                  <a href={c.links?.repos_url?.github?.[0]}>
-                    {c.links.repos_url.github[0] || "N/A"}
-                  </a>
-                </p>
-              </div>
-            )}
-            <p>
-              Current price: ${c.market_data.current_price.usd}{" "}
-              <span className="one-day-change">
-                {c.market_data.price_change_percentage_24h}
-              </span>
-            </p>
-            <p>Market cap: ${c.market_data.market_cap.usd}</p>
-            <p>Total volume: ${c.market_data.total_volume.usd}</p>
-            <p>
-              Price change(7d): {c.market_data.price_change_percentage_7d} %
-            </p>
-            <p>
-              Price change(14d): {c.market_data.price_change_percentage_14d} %
-            </p>
-            <p>Total supply: {c.market_data.total_supply}</p>
-            <p>Circulating supply: {c.market_data.circulating_supply}</p>
-            <p>
-              Max supply:{" "}
-              {c.market_data.max_supply ? c.market_data.max_supply : "N/A"}
-            </p>
-            <p>
-              All-time high: ${c.market_data.ath.usd}{" "}
-              <span>
-                {formatDate(c.market_data.ath_date.usd)} (
-                {getTimeDifference(c.market_data.ath_date.usd)} years ago){" "}
-                <br />
-                {getPriceDifference(
-                  c.market_data.current_price.usd,
-                  c.market_data.ath.usd
-                )}
-                % difference
-              </span>
-            </p>
-            <p>
-              All-time low: ${c.market_data.atl.usd}{" "}
-              <span>
-                {formatDate(c.market_data.atl_date.usd)} (
-                {getTimeDifference(c.market_data.atl_date.usd)} years ago)
-                <br />
-                {getPriceDifference(
-                  c.market_data.current_price.usd,
-                  c.market_data.atl.usd
-                )}
-                % difference
-              </span>
-            </p>
-          </div>
-          //       <p>Volume: ${l.total_volume}</p>
-        )}
+        <div>
+          {coinDetails && (
+            <div>
+              <p>
+                Name: {c.name} <img className="coin-img" src={c.image.thumb} />
+              </p>
+              <p>Id: {c.id}</p>
+              <p>Ticker: {c.symbol}</p>
+              <p>Description: {c.description.en || "N/A"}</p>
+              {c?.links && (
+                <div>
+                  <p>
+                    Homepage:{" "}
+                    <a href={c.links?.homepage?.[0]}>
+                      {" "}
+                      {c.links.homepage || "N/A"}
+                    </a>
+                  </p>
+                  <p>
+                    Whitepaper:{" "}
+                    <a href={c.links?.whitepaper?.[0]}>
+                      {c.links.whitepaper || "N/A"}
+                    </a>
+                  </p>
+                  <p>
+                    Reddit:{" "}
+                    <a href={c.links?.subreddit_url?.[0]}>
+                      {c.links.subreddit_url || "N/A"}
+                    </a>
+                  </p>
+                  <p>
+                    Github:{" "}
+                    <a href={c.links?.repos_url?.github?.[0]}>
+                      {c.links.repos_url.github[0] || "N/A"}
+                    </a>
+                  </p>
+                </div>
+              )}
+              <p>
+                Current price: ${c.market_data.current_price.usd}{" "}
+                <span className="one-day-change">
+                  {c.market_data.price_change_percentage_24h}
+                </span>
+              </p>
+              <p>Market cap: ${c.market_data.market_cap.usd}</p>
+              <p>Total volume: ${c.market_data.total_volume.usd}</p>
+              <p>
+                Price change(7d): {c.market_data.price_change_percentage_7d} %
+              </p>
+              <p>
+                Price change(14d): {c.market_data.price_change_percentage_14d} %
+              </p>
+              <p>Total supply: {c.market_data.total_supply}</p>
+              <p>Circulating supply: {c.market_data.circulating_supply}</p>
+              <p>
+                Max supply:{" "}
+                {c.market_data.max_supply ? c.market_data.max_supply : "N/A"}
+              </p>
+              <p>
+                All-time high: ${c.market_data.ath.usd}{" "}
+                <span>
+                  {formatDate(c.market_data.ath_date.usd)} (
+                  {getTimeDifference(c.market_data.ath_date.usd)} years ago){" "}
+                  <br />
+                  {getPriceDifference(
+                    c.market_data.current_price.usd,
+                    c.market_data.ath.usd
+                  )}
+                  % difference
+                </span>
+              </p>
+              <p>
+                All-time low: ${c.market_data.atl.usd}{" "}
+                <span>
+                  {formatDate(c.market_data.atl_date.usd)} (
+                  {getTimeDifference(c.market_data.atl_date.usd)} years ago)
+                  <br />
+                  {getPriceDifference(
+                    c.market_data.current_price.usd,
+                    c.market_data.atl.usd
+                  )}
+                  % difference
+                </span>
+              </p>
+              <br />
+            </div>
+          )}
+        </div>
+        {ChartComponent()}
       </div>
     );
   };
